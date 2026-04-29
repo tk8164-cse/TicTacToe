a60689eebf034a3813286642ae833c5a6ec01178
Kosaraju Tanvi <tk8164@srmist.edu.in>
2026-04-29 09:08:04 +0530

    Coded UC7: Computer Makes Random Move

--- main/java/org/example/TicTacToe/src/main/java/org/example/TicTacToe.java
+++ main/java/org/example/TicTacToe/src/main/java/org/example/TicTacToe.java
@@ -2,34 +2,40 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
 public class TicTacToe {
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+    static char computerSymbol = 'O';
     public static void main(String[] args) {
-        initializeBoard();
-        placeMove(0, 0, 'X');
-        placeMove(1, 1, 'O');
-        placeMove(2, 2, 'X');
+        System.out.println("Board before move:");
+        printBoard();
+        computerMove();
+        System.out.println("\nBoard after computer move:");
         printBoard();
     }
-    static void placeMove(int row, int col, char symbol) {
-        board[row][col] = symbol;
-    }
-    static void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = ' ';
+    static void computerMove() {
+        Random random = new Random();
+        boolean moved = false;
+        while (!moved) {
+            int row = random.nextInt(3);
+            int col = random.nextInt(3);
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                moved = true;
+                System.out.println("Computer placed 'O' at: row " + row + ", col " + col);
             }
         }
     }
     static void printBoard() {
-        System.out.println("-------------");
         for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
-            System.out.println("-------------");
         }
     }
 }
